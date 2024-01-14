52bfdf4392c7c76c09a26ed5a553ae6f68337c0e
Sele9562 <149209000+Sele9562@users.noreply.github.com>
2024-01-13 22:42:07 -0500

    Add files via upload

---
+++ Parte4finalAplicacionTeclado/src/parte4finalaplicacionteclado/Parte4finalAplicacionTeclado.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parte4finalaplicacionteclado;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Parte4finalAplicacionTeclado extends JFrame implements ActionListener{
+
+    
+    public Parte4finalAplicacionTeclado() { 
+        areaTexto = new JTextArea(10, 40);
+        JScrollPane panelDesplazable = new JScrollPane(areaTexto); 
+        JPanel panelTeclado = new JPanel(new GridLayout(4, 10)); 
+        listaPangramas = new ArrayList<>();
+        pulsacionesCorrectas = 0;
+        pulsacionesIncorrectas = 0; teclasDificultad = new HashMap<>();
+        cargarPangramas();
+        botonesTeclado = new JButton[26]; char caracterTecla = 'A';
+    
+        for (int i = 0; i < 26; i++) { 
+        botonesTeclado[i] = new JButton(Character.toString(caracterTecla)); 
+        botonesTeclado[i].addActionListener(this); panelTeclado.add(botonesTeclado[i]); caracterTecla++;
+        }
+        setTitle("Aplicación de Escritura al Tacto"); 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        setLayout(new BorderLayout());
+        add(panelDesplazable, BorderLayout.CENTER); 
+        add(panelTeclado, BorderLayout.SOUTH);
+        pack(); 
+        setLocationRelativeTo(null); 
+        setVisible(true);
+        mostrarPangramaAleatorio();
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
